f2eae8a574ee0d202746a0bb6833ae770dd10272
jiashenC <caojiashen24@gmail.com>
2017-09-26 12:02:17 -0400

    add clsfly stats

--- parallel/keras_quick_start.py
+++ parallel/keras_quick_start.py
@@ -1,6 +1,6 @@
 from keras.models import load_model
 from keras.utils import plot_model
 
-model = load_model('/home/jiashen/weights/batch_4_noaug/199_epoch-0.2510_loss-0.9403_acc-6.5269_val_loss-0.3061_val_acc.hdf5')
+model = load_model('/home/jiashenc/0000_epoch-4.0079_loss-0.0253_acc-4.1435_val_loss-0.0266_val_acc.hdf5')
 
 print model.summary()
