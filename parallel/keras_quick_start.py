b051676a3f92f7bebb4dbbfc6650266fcf4b8505
jiashenC <caojiashen24@gmail.com>
2017-09-27 19:58:21 -0400

    add inference time for CNN

--- parallel/keras_quick_start.py
+++ parallel/keras_quick_start.py
@@ -1,6 +1,16 @@
 from keras.models import load_model
 from keras.utils import plot_model
+import numpy as np
+import time
 
-model = load_model('/home/jiashen/weights/batch_4_noaug/199_epoch-0.2510_loss-0.9403_acc-6.5269_val_loss-0.3061_val_acc.hdf5')
+model = load_model('/home/jiashen/weights/clsfybatch_4/0000_epoch-4.0079_loss-0.0253_acc-4.1435_val_loss-0.0266_val_acc.hdf5')
 
-print model.summary()
+test_x = np.random.rand(8192)
+
+# pop the last three layers from training 
+for _ in range(3):
+    model.pop()
+
+start = time.time()
+model.predict(np.array([test_x]))
+print 'FC layer {:.3f}s'.format(time.time() - start)
