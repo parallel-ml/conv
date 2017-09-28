ac43771d6999795970e953e487171db1fb03e87a
Jiashen Cao <caojiashen24@gmail.com>
2017-09-28 11:57:35 -0400

    add pi usage

--- siamese_clsfy/keras_quick_start.py
+++ siamese_clsfy/keras_quick_start.py
@@ -6,9 +6,9 @@ from memory_profiler import profile
 
 @profile
 def main():
-    model = load_model('/home/jiashen/weights/batch_4_noaug/199_epoch-0.2510_loss-0.9403_acc-6.5269_val_loss-0.3061_val_acc.hdf5')
+    model = load_model('/home/pi/weights/batch_4_aug/199_epoch-5.2804_loss-0.1080_acc-5.9187_val_loss-0.0662_val_acc.hdf5')
 
-    test_x = np.random.rand(12, 16, 20)
+    test_x = np.random.rand(12, 16, 3)
 
     # pop the last three layers from training 
     for _ in range(3):
@@ -20,5 +20,5 @@ def main():
 
 
 if __name__ == '__main__':
-    print '+++++++++++++++++++++optical flow++++++++++++++++++++++++++++'
+    print '+++++++++++++++++++++image++++++++++++++++++++++++++++'
     main()
