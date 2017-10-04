067e892da418c982ea6fe691a39989c755bcdd18
jiashenC <caojiashen24@gmail.com>
2017-10-03 20:38:00 -0400

    finish 4 maxpooling layers

--- siamese_clsfy/maxpool.py
+++ siamese_clsfy/maxpool.py
@@ -1,25 +1,39 @@
 import numpy as np
-from keras.layers.pooling import GlobalAveragePooling2D, MaxPooling2D
-from keras.models import Sequential
+from keras.layers.pooling import MaxPooling1D
+from keras.layers import Input
+from keras.layers.merge import Concatenate
+from keras.models import Sequential, Model
 
 
 def main():
+    test_x = np.random.rand(64, 256)
+
     def load(*args, **kwargs):
         N = kwargs['N']
-        model = Sequential()
-        model.add(GlobalAveragePooling2D(data_format=(N, 256)))
-        model.add(MaxPooling2D(pool_size=(N / 2, 256), strides=N / 2))
-        model.add(MaxPooling2D(pool_size=(N / 4, 256), strides=N / 4))
-        model.add(MaxPooling2D(pool_size=(N / 8, 256), strides=N / 8))
+
+        input = Input(shape=test_x.shape, name='input')
+
+        max1 = MaxPooling1D(pool_size=N, strides=N)(input)
+        max2 = MaxPooling1D(pool_size=N / 2, strides=N / 2)(input)
+        max3 = MaxPooling1D(pool_size=N / 4, strides=N / 4)(input)
+        max4 = MaxPooling1D(pool_size=N / 8, strides=N / 8)(input)
+
+        mrg = Concatenate(axis=1)([max1, max2, max3, max4])
+
+        model = Model(input=input, outputs=mrg)
+
+        print model.summary()
+
         return model
 
     model = load(N=64)
-    test_x = np.random.rand((64, 256))
 
     def forward():
-        model.predict(np.array([test_x]))
+        return model.predict(np.array([test_x]))
 
-    forward()
+    test_y = forward()
+
+    print test_y.shape
 
 
 if __name__ == '__main__':
