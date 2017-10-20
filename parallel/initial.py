15156ac4505ad9720fb114c487d09c0d01cfb8d3
jiashenC <caojiashen24@gmail.com>
2017-10-20 00:10:09 -0400

    fix multithreading functionality at initializer side

--- parallel/initial.py
+++ parallel/initial.py
@@ -1,16 +1,25 @@
-import json
-from multiprocessing import Pool, Queue
+from multiprocessing import Queue
+from threading import Thread
 
 import avro.ipc as ipc
 import avro.protocol as protocol
 import cv2
 import numpy as np
+import yaml
 
 PROTOCOL = protocol.parse(open('resource/image.avpr').read())
 
+global spatial_q, temporal_q
 
-def send_request(bytestr, addr_queue, mode):
-    addr = addr_queue.get()
+
+def send_request(bytestr, mode='spatial'):
+    queue = None
+    if mode == 'spatial':
+        queue = spatial_q
+    else:
+        queue = temporal_q
+
+    addr = queue.get()
     client = ipc.HTTPTransceiver(addr[0], addr[1])
     requestor = ipc.Requestor(PROTOCOL, client)
 
@@ -24,12 +33,10 @@ def send_request(bytestr, addr_queue, mode):
         output = output.reshape(1, 51)
 
     client.close()
-    addr_queue.put(addr)
+    queue.put(addr)
 
 
-def master(ip):
-    pool = Pool(5)
-
+def master():
     frame_count = 4
     frame_width = 16
     frame_height = 12
@@ -42,32 +49,30 @@ def master(ip):
         ret, frame = 'unknown', np.random.rand(12, 16, 3) * 255
         frame = frame.astype(dtype=np.uint8)
 
-        pool.apply_async(send_request, (frame.tobytes(), ip['spatial'], 'spatial'))
+        Thread(target=send_request, args=(frame.tobytes(), 'spatial')).start()
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        if not frame0:
+        if frame0 is not None:
             flows[..., (index - 1) * 2:(index - 1) * 2 + 2] = cv2.calcOpticalFlowFarneback(frame0, frame, None, 0.5, 3,
                                                                                            4, 3, 5, 1.1, 0)
 
         frame0 = frame
         index += 1
 
-    pool.apply_async(send_request, (frame.tobytes(), ip['temporal'], 'temporal'))
+    Thread(target=send_request, args=(flows.tobytes(), 'temporal')).start()
 
 
 def main():
-    ip = dict()
+    global spatial_q, temporal_q
     with open('resource/ip') as file:
-        address = json.load(file)
+        address = yaml.safe_load(file)
         spatial_q, temporal_q = Queue(), Queue()
-        for ip in address['spatial']:
-            spatial_q.put(ip)
-        for ip in address['temporal']:
-            temporal_q.put(ip)
-        ip['spatial'] = spatial_q
-        ip['temporal'] = temporal_q
-    master(ip)
+        for addr in address['spatial']:
+            spatial_q.put(addr)
+        for addr in address['temporal']:
+            temporal_q.put(addr)
+    master()
 
 
 if __name__ == '__main__':
