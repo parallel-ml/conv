83c2b29b7079a92767d6d83d7a9a6b3b70deeaa1
jiashenC <caojiashen24@gmail.com>
2017-11-02 16:41:38 -0400

    edit README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # parallel-ml
+
+* parallel directory stores distributed system setup.
+* siamese_clsfy stores all code for machine learning model. 
--- siamese_clsfy/keras_quick_start.py
+++ siamese_clsfy/keras_quick_start.py
@@ -1,49 +1,20 @@
 import numpy as np
-from keras.layers import MaxPooling1D, Dense, Activation, BatchNormalization, Input
+from keras.layers.convolutional import Conv2D
+from keras.layers import MaxPooling1D, Dense, Activation, BatchNormalization, Input, Flatten
 from keras.layers.merge import Concatenate
 from keras.models import load_model, Model, Sequential
 from memory_profiler import profile
 
 from util.output import title, timer, avg_timer, subtitle
 
-TIMING = False
 
-
-@title('maxpooling layers')
-@profile
 def main():
-    optical_flow()
-    image()
+    run_spatial()
+    run_temporal()
+    run_maxpool()
 
 
-def load_helper(mode):
-    if mode == 'optical flow':
-        return load(
-            path='/home/pi/weights/batch_4_noaug/199_epoch-0.2510_loss-0.9403_acc-6.5269_val_loss-0.3061_val_acc.hdf5')
-    elif mode == 'image':
-        return load(
-            path='/home/pi/weights/batch_4_aug/199_epoch-5.2804_loss-0.1080_acc-5.9187_val_loss-0.0662_val_acc.hdf5')
-    elif mode == 'maxpool':
-        N = 100
-        while N <= 2000:
-            @subtitle(str(N) +  ' image frames')
-            @profile
-            def loadmax(N):
-                return maxpool(N=N)
-
-            loadmax(N)
-
-            N += 100
-    elif mode == 'fc':
-        return fc()
-
-
-@timer('load model', timing=TIMING)
-def load(*args, **kwargs):
-    return load_model(kwargs['path'])
-
-
-def fc(*args, **kwargs):
+def run_fc():
     model = Sequential()
     model.add(Dense(4096, input_shape=(7680,)))
     model.add(BatchNormalization(input_shape=(4096,)))
@@ -59,9 +30,10 @@ def fc(*args, **kwargs):
     return model
 
 
-def maxpool(*args, **kwargs):
-    N = kwargs['N']
+def run_maxpool():
+    test_x = np.random.rand(16, 256)
 
+    N = 16
     input = Input(shape=(N, 256), name='input')
 
     max1 = MaxPooling1D(pool_size=N, strides=N)(input)
@@ -70,51 +42,67 @@ def maxpool(*args, **kwargs):
     max4 = MaxPooling1D(pool_size=N / 8, strides=N / 8)(input)
 
     mrg = Concatenate(axis=1)([max1, max2, max3, max4])
+    flat = Flatten()(mrg)
+    model = Model(input=input, outputs=flat)
+    output = model.predict(np.array([test_x]))
+    print output
 
-    model = Model(input=input, outputs=mrg)
+
+def run_temporal():
+    model = load_temporal()
+    test_x = np.random.rand(12, 16, 20)
+    # pop the last three layers used by training
+    for _ in range(3):
+        model.pop()
+    output = model.predict(np.array([test_x]))
+    print output
+
+
+def run_spatial():
+    model = load_spatial()
+    test_x = np.random.rand(12, 16, 3)
+    # pop the last three layers from training
+    for _ in range(3):
+        model.pop()
+    output = model.predict(np.array([test_x]))
+    print output
+
+
+def load_spatial():
+    return load_cnn(nb_channel=3)
+
+
+def load_temporal():
+    return load_cnn(nb_channel=20)
+
+
+def load_cnn(nb_class=1000, bias=True, act='relu', bn=True, dropout=False, moredense=False, nb_filter=256,
+             nb_channel=3, clsfy_act='softmax'):
+    # input image is 16x12, RGB(x3)
+    model = Sequential()
+    model.add(Conv2D(nb_filter, (5, 5), use_bias=bias, padding='same', input_shape=(12, 16, nb_channel)))
+    model.add(Activation(act, name='relu_1'))
+    if bn:
+        model.add(BatchNormalization())
+
+    model.add(Conv2D(nb_filter, (3, 3), use_bias=bias, padding='same'))
+    model.add(Activation(act, name='relu_2'))
+    if bn:
+        model.add(BatchNormalization())
+
+    model.add(Conv2D(nb_filter, (3, 3), use_bias=bias, padding='same'))
+    model.add(Activation(act, name='relu_3'))
+    if bn:
+        model.add(BatchNormalization())
+
+    model.add(Flatten())
+    # now model.output_shape == (None, 24576)
+    model.add(Dense(256, input_dim=24576, use_bias=bias, name='dense_4'))
+    if bn:
+        model.add(BatchNormalization())
+    model.add(Activation(act, name='relu_4'))
 
     return model
-
-
-@avg_timer('inference', timing=TIMING)
-def forward(*args, **kwargs):
-    model = kwargs['model']
-    test_x = kwargs['test_x']
-    model.predict(np.array([test_x]))
-
-
-@title('optical flow (temporal)')
-def optical_flow():
-    model = load(
-        path='/home/jiashen/weights/batch_4_noaug/199_epoch-0.2510_loss-0.9403_acc-6.5269_val_loss-0.3061_val_acc.hdf5')
-
-    test_x = np.random.rand(12, 16, 20)
-
-    # pop the last three layers from training
-    for _ in range(3):
-        model.pop()
-
-    output = model.predict(np.array([test_x]))
-    print output.shape
-
-    forward(model=model, test_x=test_x)
-
-
-@title('single frame (spatial)')
-def image():
-    model = load(
-        path='/home/jiashen/weights/batch_4_aug/199_epoch-5.2804_loss-0.1080_acc-5.9187_val_loss-0.0662_val_acc.hdf5')
-
-    test_x = np.random.rand(12, 16, 3)
-
-    # pop the last three layers from training
-    for _ in range(3):
-        model.pop()
-
-    output = model.predict(np.array([test_x]))
-    print output.shape
-
-    forward(model=model, test_x=test_x)
 
 
 if __name__ == '__main__':
