b7b806e99bedbb53411800ad598a3e8dd22e1cb7
pi <mwoodward@gatech.edu>
2017-11-16 16:16:42 -0500

    change ip address used as the server by client

--- client.py
+++ client.py
@@ -10,15 +10,15 @@ import sys
 PROTOCOL = protocol.parse(open('image.avpr').read())
 
 def ping(size):
-    server_addr = ('198.162.1.2', 8000)
+    server_addr = ('192.168.1.2', 8000)
     client = ipc.HTTPTransceiver(server_addr[0], server_addr[1])
     requestor = ipc.Requestor(PROTOCOL, client)
 
     # fill in the Message record and send it
     message = dict()
     message['image'] ='1' * (size*1000)
-
-    data = requestor.Request('process', message)
+    
+    data = requestor.request('process', message)
     client.Close()
 
 
