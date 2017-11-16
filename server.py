fd7e87860f9de869e53555024fbf2c6e1ae50cde
pi <mwoodward@gatech.edu>
2017-11-16 17:02:08 -0500

    Swap for working version from very old commit

--- server.py
+++ server.py
@@ -1,56 +1,58 @@
-import time
-import array
-import io
-import json
-import struct
-import avro.datafile
-import avro.schema
-import avro.ipc as ipc
-import avro.protocol as protocol
-# import http.server
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from SocketServer import ThreadingMixIn
 
+import avro.ipc as ipc
+import avro.protocol as protocol
+import avro.schema as schema
+import matplotlib
 
+matplotlib.use('Agg')
+import time
 
-PROTOCOL = protocol.parse(open('image.avpr').read())
+PROTOCOL = protocol.parse(open('resource/image.avpr').read())
+
 
 class ImageResponder(ipc.Responder):
     def __init__(self):
         ipc.Responder.__init__(self, PROTOCOL)
-        print "start init"
 
-    def Invoke(self, msg, req):
-        print "start invoke"
-        if msg.name == 'process':
-            image = req['image']
-
-            response = []
-            response.append("1")
-            return response
-
+    def invoke(self, msg, req):
+        """
+        automatically called by do_POST method
+        :param msg: message name
+        :param req: request sent by client
+        :return:
+        """
+        if msg.name == 'procimage':
+            return 'GET'
         else:
-            print "Schema does not match"
+            raise schema.AvroException('unexpected message:', msg.getname())
 
 
 class ImageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        """
+        this method will be called once the server gets the request
+        :return:
+        """
         self.responder = ImageResponder()
         call_request_reader = ipc.FramedReader(self.rfile)
         call_request = call_request_reader.read_framed_message()
         resp_body = self.responder.respond(call_request)
         self.send_response(200)
-        self.send_header('Content=Type', 'avro/binary')
+        self.send_header('Content-Type', 'avro/binary')
         self.end_headers()
         resp_writer = ipc.FramedWriter(self.wfile)
         resp_writer.write_framed_message(resp_body)
 
-server_addr = ('0.0.0.0', 8000)
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """handle requests in separate thread"""
+
+
+server_addr = ('0.0.0.0', 12345)
 
 if __name__ == '__main__':
-    server = HTTPServer(server_addr, ImageHandler)
+    server = ThreadedHTTPServer(server_addr, ImageHandler)
     server.allow_reuse_address = True
-
-    print "start testing"
-
     server.serve_forever()
