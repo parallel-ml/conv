07df46fae04131dd014abd07770d6b1a8a6670cf
pi <ramyad.hadidi@gmail.com>
2017-11-17 13:10:14 -0500

    CNN data

--- siamese_clsfy/run_cnn_with_perf.sh
+++ siamese_clsfy/run_cnn_with_perf.sh
@@ -11,7 +11,8 @@ OS="page-faults,dTLB-load-misses,dTLB-store-misses,iTLB-load-misses"
 
 ARM='"armv7_cortex_a7/br_immed_retired/","armv7_cortex_a7/br_mis_pred/","armv7_cortex_a7/br_pred/","armv7_cortex_a7/br_return_retired/","armv7_cortex_a7/bus_access/","armv7_cortex_a7/bus_cycles/","armv7_cortex_a7/cid_write_retired/","armv7_cortex_a7/cpu_cycles/","armv7_cortex_a7/exc_return/","armv7_cortex_a7/exc_taken/","armv7_cortex_a7/inst_retired/","armv7_cortex_a7/inst_spec/","armv7_cortex_a7/l1d_cache/","armv7_cortex_a7/l1d_cache_refill/","armv7_cortex_a7/l1d_cache_wb/","armv7_cortex_a7/l1d_tlb_refill/","armv7_cortex_a7/l1i_cache/","armv7_cortex_a7/l1i_cache_refill/","armv7_cortex_a7/l1i_tlb_refill/","armv7_cortex_a7/l2d_cache/","armv7_cortex_a7/l2d_cache_refill/","armv7_cortex_a7/l2d_cache_wb/","armv7_cortex_a7/ld_retired/","armv7_cortex_a7/mem_access/","armv7_cortex_a7/memory_error/","armv7_cortex_a7/pc_write_retired/","armv7_cortex_a7/st_retired/","armv7_cortex_a7/sw_incr/","armv7_cortex_a7/ttbr_write_retired/,"armv7_cortex_a7/unaligned_ldst_retired/"'
 
-limits=( 10 20 40 60 80 100 120 160 200 240 320) 
+#limits=( 10 20 40 60 80 100 120 160 200 240 320) 
+limits=( 220 250  400 440 480 500) 
 
 for i in "${limits[@]}"
 do
