fb2aaa20df371d2b7b52d98dfd24bd885c2482a7
jiashenC <caojiashen24@gmail.com>
2017-11-21 18:45:15 -0500

    change alexnet whole model

--- alexnet_parallel/single.py
+++ alexnet_parallel/single.py
@@ -5,7 +5,7 @@ import time
 
 def main():
     model = ml.whole()
-    test_x = np.random.rand(224, 224, 3)
+    test_x = np.random.rand(220, 220, 3)
     start = time.time()
     for _ in range(50):
         model.predict(np.array([test_x]))
