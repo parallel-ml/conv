bcd0ab6cc71889fb6a5c2417f871f9b3b69e5617
jiashenC <caojiashen24@gmail.com>
2017-11-20 20:50:31 -0500

    add distributed vgg16 and update ip talbe

--- vgg16_parallel/model.py
+++ vgg16_parallel/model.py
@@ -40,3 +40,70 @@ def vgg16(input_shape=(224, 224, 3)):
     x = Dense(1000, activation='softmax', name='predictions')(x)
 
     return Model(img_input, x, name='vgg16')
+
+
+def block1():
+    img_input = Input(shape=(224, 224, 3))
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    model = Model(img_input, x)
+    print model.output
+    return model
+
+
+def block2():
+    img_input = Input(shape=(112, 112, 64))
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(img_input)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    model = Model(img_input, x)
+    return model
+
+
+def block3():
+    img_input = Input(shape=(56, 56, 128))
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(img_input)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    model = Model(img_input, x)
+    return model
+
+
+def block4():
+    img_input = Input(shape=(28, 28, 256))
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(img_input)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    model = Model(img_input, x)
+    return model
+
+
+def block5():
+    img_input = Input(shape=(14, 14, 512))
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(img_input)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    x = Flatten(name='flatten')(x)
+    model = Model(img_input, x)
+    return model
+
+
+def block6():
+    img_input = Input(shape=(25088,))
+    x = Dense(2048, activation='relu', name='fc1')(img_input)
+    model = Model(img_input, x)
+    return model
+
+
+def block7():
+    img_input = Input(shape=(4096,))
+    x = Dense(4096, activation='relu', name='fc2')(img_input)
+    x = Dense(1000, activation='softmax', name='predictions')(x)
+    model = Model(img_input, x)
+    return model
+
+
