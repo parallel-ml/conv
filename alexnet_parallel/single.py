caec37356473262e737492bc5fa9ffddf90ef9ec
jiashenC <caojiashen24@gmail.com>
2017-11-21 19:33:01 -0500

    single alexnet

--- alexnet_parallel/single.py
+++ alexnet_parallel/single.py
@@ -4,8 +4,26 @@ import time
 
 
 def main():
-    model = ml.whole()
+    model = ml.node_4_block1()
     test_x = np.random.rand(220, 220, 3)
+    start = time.time()
+    for _ in range(50):
+        model.predict(np.array([test_x]))
+    print '{:.3f} sec'.format((time.time() - start) / 50)
+
+    del model
+
+    model = ml.node_4_block2()
+    test_x = np.random.rand(4608)
+    start = time.time()
+    for _ in range(50):
+        model.predict(np.array([test_x]))
+    print '{:.3f} sec'.format((time.time() - start) / 50)
+
+    del model
+
+    model = ml.node_4_block3()
+    test_x = np.random.rand(4096)
     start = time.time()
     for _ in range(50):
         model.predict(np.array([test_x]))
