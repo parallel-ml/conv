3d49fc93f0751533cc87e7be274d4598c1dab21b
Jiashen Cao <caojiashen24@gmail.com>
2018-01-12 14:40:09 -0500

    fix channel wise inference

--- cnn/quickstart/channel.py
+++ cnn/quickstart/channel.py
@@ -1,26 +1,25 @@
 from keras.layers import Conv2D, Input, Lambda
 from keras.layers.merge import Concatenate
 from keras.models import Model
-import keras.backend as K
 import numpy as np
 
 
-def split(X):
+def split(X, filters):
     """ return a list of 3D tensor split by channel """
-    return Lambda(lambda x: [x[:, :, :, k:k + 1] for k in range(K.int_shape(x)[-1])])(X)
+    return Lambda(lambda x: [x for _ in range(filters)])(X)
 
 
 def merge(tensors):
-    return Concatenate(axis=-1)(tensors)
+    return Concatenate()(tensors)
 
 
-def conv(tensors, filters, kernal, stride, padding):
-    return [Conv2D(filters=filters, kernel_size=kernal, strides=stride, padding=padding)(x) for x in tensors]
+def conv(tensors, kernal, stride, padding):
+    return [Conv2D(filters=1, kernel_size=kernal, strides=stride, padding=padding)(x) for x in tensors]
 
 
 def forward(data, filters, kernal, stride=(1, 1), padding='same'):
     X = Input(data.shape)
-    output = merge(conv(split(X), filters, kernal, stride, padding))
+    output = merge(conv(split(X, filters), kernal, stride, padding))
     model = Model(X, output)
     return model.predict(np.array([data]))
 
