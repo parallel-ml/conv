d1e32df01e6043596a5372541e13b4d7da442a7c
Jiashen Cao <caojiashen24@gmail.com>
2018-01-19 13:03:25 -0500

    fix spatial 2x2 split

--- cnn/quickstart/xy.py
+++ cnn/quickstart/xy.py
@@ -5,20 +5,31 @@ import keras.backend as K
 import numpy as np
 
 
-def split_xy(X, kernal, stride):
-    """ return a list of 3D tensor split by x and y """
-    w, h = kernal
+def split_xy_2(X, kernal, stride):
+    """
+        return a list of 3D tensor split by x and y
+
+        output_size = floor((input_size - kernal_size + 2 * padding) / stride) + 1
+        left_part_size = output_size / 2
+        right_part_size = output_size - left_part_size
+        left_input_size = (left_part_size - 1) * stride + kernal_size
+        right_input_size = input_size - ...
+    """
+    wk, hk = kernal
     ws, hs = stride
     _, W, H, _ = K.int_shape(X)
-    wb = ((W / ws) / 2) * ws + w + 1
-    hb = ((H / hs) / 2) * hs + h + 1
+    ow, oh = (W - wk) / ws + 1, (H - hk) / hs + 1
+    wl, wr = ow / 2, ow - ow / 2
+    wlb, wrb = (wl - 1) * ws + wk, W - ((wr - 1) * ws + wk)
+    hl, hr = oh / 2, oh - oh / 2
+    hlb, hrb = (hl - 1) * hs + hk, H - ((hr - 1) * hs + hk)
     return Lambda(
         lambda x:
-        [x[:, :wb, :hb, :], x[:, wb - (w - ws):, :hb, :], x[:, :wb, hb - (h - hs):, :], x[:, wb - (w - ws):, hb - (h - hs):, :]]
+        [x[:, :wlb, :hlb, :], x[:, wrb:, :hlb, :], x[:, :wlb, hrb:, :], x[:, wrb:, hrb:, :]]
     )(X)
 
 
-def merge(tensors):
+def merge_2(tensors):
     up = Concatenate(axis=1)(tensors[:2])
     down = Concatenate(axis=1)(tensors[2:])
     return Concatenate(axis=2)([up, down])
@@ -30,6 +41,6 @@ def conv(tensors, filters, kernal, stride, padding):
 
 def forward(data, filters, kernal, stride=(1, 1), padding='valid'):
     X = Input(data.shape)
-    output = merge(conv(split_xy(X, kernal, stride), filters, kernal, stride, padding))
+    output = merge_2(conv(split_xy_2(X, kernal, stride), filters, kernal, stride, padding))
     model = Model(X, output)
     return model.predict(np.array([data]))
