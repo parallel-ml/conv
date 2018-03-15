c1b642bc275c73c7bb8d8797a14060ff5aa2d8c1
Jiashen Cao <caojiashen24@gmail.com>
2018-03-15 11:24:25 -0400

    separable unit with filter split

--- tests/test_separable.py
+++ tests/test_separable.py
@@ -59,3 +59,62 @@ def test_original():
         'kernal: ', (3, 3),
         'stride: ', (2, 2),
     )
+
+
+def test_filter():
+    X = Input([256, 256, 3])
+    conv = SeparableConv2D(10, (3, 3))(X)
+    model = Model(X, conv)
+
+    conv = separable.filter(X, 10, (3, 3))
+    test_model = Model(X, conv)
+    print test_model.summary()
+    assert model.output_shape[1:] == test_model.output_shape[1:] \
+        and model.count_params() + 20 == test_model.count_params(), (
+        'expected: ', model.output_shape[1:], ' param: ', model.count_params(),
+        'actual: ', test_model.output_shape[1:], ' param: ', test_model.count_params(),
+        'kernal: ', (3, 3),
+        'stride: ', (1, 1),
+    )
+
+    X = Input([256, 256, 3])
+    conv = SeparableConv2D(10, (3, 3), strides=(2, 2))(X)
+    model = Model(X, conv)
+
+    conv = separable.filter(X, 10, (3, 3), strides=(2, 2))
+    test_model = Model(X, conv)
+    assert model.output_shape[1:] == test_model.output_shape[1:] \
+        and model.count_params() + 20 == test_model.count_params(), (
+        'expected: ', model.output_shape[1:], ' param: ', model.count_params(),
+        'actual: ', test_model.output_shape[1:], ' param: ', test_model.count_params(),
+        'kernal: ', (3, 3),
+        'stride: ', (2, 2),
+    )
+
+    X = Input([256, 256, 3])
+    conv = SeparableConv2D(10, (3, 3), padding='same')(X)
+    model = Model(X, conv)
+
+    conv = separable.filter(X, 10, (3, 3), padding='same')
+    test_model = Model(X, conv)
+    assert model.output_shape[1:] == test_model.output_shape[1:] \
+        and model.count_params() + 20 == test_model.count_params(), (
+        'expected: ', model.output_shape[1:], ' param: ', model.count_params(),
+        'actual: ', test_model.output_shape[1:], ' param: ', test_model.count_params(),
+        'kernal: ', (3, 3),
+        'stride: ', (1, 1),
+    )
+
+    X = Input([256, 256, 3])
+    conv = SeparableConv2D(10, (3, 3), strides=(2, 2), padding='same')(X)
+    model = Model(X, conv)
+
+    conv = separable.filter(X, 10, (3, 3), strides=(2, 2), padding='same')
+    test_model = Model(X, conv)
+    assert model.output_shape[1:] == test_model.output_shape[1:] \
+        and model.count_params() + 20 == test_model.count_params(), (
+        'expected: ', model.output_shape[1:], ' param: ', model.count_params(),
+        'actual: ', test_model.output_shape[1:], ' param: ', test_model.count_params(),
+        'kernal: ', (3, 3),
+        'stride: ', (2, 2),
+    )
--- utils/separable.py
+++ utils/separable.py
@@ -3,14 +3,35 @@
     by using its normal Conv2D layer. Instead of treating it as black
     box, we want to split it internally.
 """
-from keras.layers import Conv2D, Lambda, Concatenate
+from keras.layers import Conv2D, Lambda, Concatenate, Add, Activation
 import keras.backend as K
 
 
 def original(X, filters, kernal, strides=(1, 1), padding='valid', activation='relu'):
+    """
+        We assume this is the original implementation by Xception author.
+        We have # channel kernal run depth wise convolution and concatenate
+        output to a single tensor. Then run a spatial wise convolution based
+        on the filter #.
+    """
     channel = K.int_shape(X)[-1]
     X = Lambda(lambda x: [x[:, :, :, k:k+1] for k in range(channel)])(X)
     X = [Conv2D(1, kernal, strides=strides, padding=padding, use_bias=False)(x) for x in X]
     X = Concatenate(axis=-1)(X)
     X = Conv2D(filters, (1, 1), padding=padding, activation=activation)(X)
+    return X
+
+
+def filter(X, filters, kernal, strides=(1, 1), padding='valid', activation='relu'):
+    """
+        For filter function, the only difference is we do not concatenate the
+        output from first Conv2D layer. Instead, it will run parallel spatial
+        convolution and add together.
+    """
+    channel = K.int_shape(X)[-1]
+    X = Lambda(lambda x: [x[:, :, :, k:k + 1] for k in range(channel)])(X)
+    X = [Conv2D(1, kernal, strides=strides, padding=padding, use_bias=False)(x) for x in X]
+    X = [Conv2D(filters, (1, 1), padding=padding)(x) for x in X]
+    X = Add()(X)
+    X = Activation(activation)(X)
     return X
